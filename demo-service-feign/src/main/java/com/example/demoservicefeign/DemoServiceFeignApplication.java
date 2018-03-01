4e219c8c29aea36f77f3a67a0a0f846c614f41dd
zengxc <zengxc@jpush.cn>
2018-03-01 21:15:14 +0800

    update demo-project 2018/3/1 21:16

--- demo-service-feign/src/main/java/com/example/demoservicefeign/DemoServiceFeignApplication.java
+++ demo-service-feign/src/main/java/com/example/demoservicefeign/DemoServiceFeignApplication.java
@@ -1,12 +1,9 @@
 package com.example.demoservicefeign;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
 @EnableFeignClients
---
+++ demo-service-zuul/src/main/java/com/example/demoservicezuul/filter/MyFilter.java
@@ -0,0 +1,67 @@
+package com.example.demoservicezuul.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+/**
+ * @desc:
+ * @author: zengxc
+ * @date: 2018/3/1
+ */
+@Slf4j
+@Component
+public class MyFilter extends ZuulFilter {
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext reqContext = RequestContext.getCurrentContext();
+        HttpServletRequest request = reqContext.getRequest();
+        log.info(String.format("%s >>> %s", request.getMethod(), request.getRequestURL().toString()));
+        String accessToken = request.getParameter("token");
+        if (accessToken == null){
+            log.warn("token is empty");
+            reqContext.setSendZuulResponse(false);
+            reqContext.setResponseStatusCode(401);
+            try {
+                reqContext.getResponse().getWriter().write("token is empty");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+        log.info("ok");
+        return null;
+    }
+
+    /**
+     *   filterType：返回一个字符串代表过滤器的类型，在zuul中定义了四种不同生命周期的过滤器类型，具体如下：
+         pre：路由之前
+         routing：路由之时
+         post： 路由之后
+         error：发送错误调用
+         filterOrder：过滤的顺序
+         shouldFilter：这里可以写逻辑判断，是否要过滤，本文true,永远过滤。
+         run：过滤器的具体逻辑。可用很复杂，包括查sql，nosql去判断该请求到底有没有权限访问。
+     */
+}
