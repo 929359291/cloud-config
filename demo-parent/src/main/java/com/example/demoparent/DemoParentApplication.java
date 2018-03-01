5fcc007146174dfa5309cd987bf0581c4d12f0ec
zengxc <zengxc@jpush.cn>
2018-03-01 21:19:28 +0800

    update demo-project 2018/3/1 21:16

---
+++ demo-parent/src/main/java/com/example/demoparent/DemoParentApplication.java
@@ -0,0 +1,12 @@
+package com.example.demoparent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoParentApplication.class, args);
+	}
+}
